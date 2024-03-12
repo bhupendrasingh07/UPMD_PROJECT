56d4092d97d7f08c4ed1dd195dabfc67dec1e475
Bhupendra singh <100069107+bhupendrasingh07@users.noreply.github.com>
2024-03-12 09:35:14 +0530

    changes in dotenv file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api/v1':'https://upmd-project.onrender.com'
+    }
+  },
   plugins: [react()],
 })
